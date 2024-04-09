36e48c8c2d36c4ee5d1dc5e5810ae3d64b5c5e63
LivhuwaniKM <lkmasindi1208@gmail.com>
2024-04-09 16:06:11 +0200

    Liskov Substitution Principle Demo

--- LiskovSubstitution/Program.cs
+++ LiskovSubstitution/Program.cs
@@ -4,7 +4,62 @@
     {
         public static void Main()
         {
+            Employee developer = new Developer();
+            Employee manager = new Manager();
 
+            HR hr = new();
+
+            var output =
+                $"===================================\n" +
+                $"Liskov Substitution Principle Demo\n" +
+                $"===================================\n" +
+                $"Role 1:\n" +
+                $"{hr.AssignRole(developer)}\n\n" +
+                $"Role 2:\n" +
+                $"{hr.AssignRole(manager)}";
+
+            Console.WriteLine(output);
+        }
+    }
+
+    public abstract class Employee
+    {
+        public abstract string GetRole();
+        public abstract string PerformRoleSpecificTask();
+    }
+
+    public class Developer : Employee
+    {
+        public override string GetRole()
+        {
+            return "Developer";
+        }
+
+        public override string PerformRoleSpecificTask()
+        {
+            return "Writing code";
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public override string GetRole()
+        {
+            return "Manager";
+        }
+
+        public override string PerformRoleSpecificTask()
+        {
+            return "Managing the team";
+        }
+    }
+
+    public class HR
+    {
+        public string AssignRole(Employee employee)
+        {
+            return
+                $"Role: {employee.GetRole()}\nTask: {employee.PerformRoleSpecificTask()}";
         }
     }
 }
