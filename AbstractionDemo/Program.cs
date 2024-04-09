b0b60350492a5fbd187f2c9f817185beef5c2e74
Livhuwani Masindi <lkmasindi1208@gmail.com>
2024-04-09 09:22:12 +0200

    Add files via upload

---
+++ AbstractionDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace DotNetOOPMasterClass.AbstractionDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Triangle triangle = new(22, 4);
+            Circle circle = new(6);
+            Square square = new(10);
+
+            string output =
+                $"===================================\n" +
+                $"Abstraction Demo\n" +
+                $"===================================\n" +
+                $"Calculation for triangle: {triangle.Area()} cm\n" +
+                $"Calculation for circle: {Math.Round(circle.Area(), 2)} cm\n" +
+                $"Calculation fro square: {square.Area()} cm";
+
+            Console.WriteLine(output);
+        }
+    }
+
+    public abstract class IShape
+    {
+        public abstract double Area();
+    }
+
+    public class Circle(double radius) : IShape
+    {
+        public double Radius { get; set; } = radius;
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Triangle(double tbase, double height) : IShape
+    {
+        public double Tbase { get; set; } = tbase;
+        public double Height { get; set; } = height;
+
+        public override double Area()
+        {
+            return 0.5 * Height * Tbase;
+        }
+    }
+
+    public class Square(double side) : IShape
+    {
+        public double Side { get; set; } = side;
+
+        public override double Area()
+        {
+            return Side * Side;
+        }
+    }
+}
