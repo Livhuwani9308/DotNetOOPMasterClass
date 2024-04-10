a1ddfaae3bac4440ddbecab00277295a2a689460
LivhuwaniKM <lkmasindi1208@gmail.com>
2024-04-10 11:43:19 +0200

    Dependencey inversion demo

--- DependencyInversion/Program.cs
+++ DependencyInversion/Program.cs
@@ -4,7 +4,56 @@
     {
         public static void Main()
         {
+            INotificationService emailService = new EmailNotificationService();
+            INotificationService smsService = new SmsNotificationService();
+            INotificationService pushService = new PushNotificationService();
 
+            NotificationServerClient emailServerClient = new(emailService);
+            NotificationServerClient smsServerClient = new(smsService);
+            NotificationServerClient pushServerClient = new(pushService);
+
+            Console.WriteLine(emailServerClient.SendNotification("Hello via email"));
+            Console.WriteLine(smsServerClient.SendNotification("Hello via sms"));
+            Console.WriteLine(pushServerClient.SendNotification("Hello via push notification"));
+        }
+    }
+
+    public interface INotificationService
+    {
+        string SendNotification(string message);
+    }
+
+    public class EmailNotificationService : INotificationService
+    {
+        public string SendNotification(string message)
+        {
+            return $"Sending email notification: {message}";
+        }
+    }
+
+    public class SmsNotificationService : INotificationService
+    {
+        public string SendNotification(string message)
+        {
+            return $"Sending SMS notification: {message}";
+        }
+    }
+
+    public class PushNotificationService : INotificationService
+    {
+        public string SendNotification(string message)
+        {
+            return $"Sending push notification: {message}";
+        }
+    }
+
+    public class NotificationServerClient(INotificationService notificationService)
+    {
+        private readonly INotificationService _notificationService = notificationService;
+
+        public string SendNotification(string message)
+        {
+            return _notificationService.SendNotification(message);
         }
     }
 }
